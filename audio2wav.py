d6c574c9d53afb355f1f6605e3aab1b4a8041cc2
Hayato Yamamoto <hayamamo@1-B6.local>
2024-09-29 12:09:10 +0900

    first commit

---
+++ audio2wav.py
@@ -0,0 +1,38 @@
+import pyaudio
+import numpy as np
+import scipy.io.wavfile
+
+RATE = 12000
+def record_audio(output_name):
+	CHUNK = 1024
+	FORMAT = pyaudio.paFloat32
+	CHANNELS = 1
+
+	pa = pyaudio.PyAudio()
+	stream = pa.open(rate = RATE,
+			channels = CHANNELS,
+			format = FORMAT,
+			input = True,
+			frames_per_buffer = CHUNK)
+
+	print("RECORD START")
+	print("ctrl + c : STOP RECORDING")
+
+	frame = []
+	while True:
+		try:
+			d = stream.read(CHUNK)
+			d = np.frombuffer(d, dtype=np.float32)
+			frame.append(d)
+		except KeyboardInterrupt:
+			break
+
+	stream.stop_stream()
+	stream.close()
+	pa.terminate()
+
+	frame = np.array(frame).flatten()
+	print("STOP {} Samples {:.2f}s".format(frame.size, frame.size/RATE))
+	scipy.io.wavfile.write(output_name, RATE, frame)
+
+record_audio("test.wav")
---
+++ test.py
@@ -0,0 +1,5 @@
+import mlx_whisper
+import pyaudio
+
+text = mlx_whisper.transcribe('test.wav', path_or_hf_repo="mlx-community/whisper-large-v3-mlx")["text"]
+print(text)
